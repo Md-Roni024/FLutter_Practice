3fa9dfd89f7562a036e4d999c6c715dc0a246a35
Roni Hossain <roni.cse024@gmail.com>
2023-11-10 16:26:05 +0600

    Add Icon Button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -21,10 +21,11 @@ class StartScreen extends StatelessWidget {
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
               onPressed: () {},
               style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-              child: const Text('Start Quiz'))
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start Quiz'))
         ],
       ),
     );
