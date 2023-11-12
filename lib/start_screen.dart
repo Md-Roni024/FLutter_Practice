144484ab33095656a998c2fb6309e121d9b6d957
Roni Hossain <roni.cse024@gmail.com>
2023-11-12 14:40:15 +0600

    Pass switchScreen function to startscreen then execute by using button

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
-
+  const StartScreen(this.startQuiz, {super.key});
+  final void Function() startQuiz;
   @override
   Widget build(context) {
     return Center(
@@ -33,7 +33,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-              onPressed: () {},
+              onPressed: startQuiz,
               style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
               icon: const Icon(Icons.arrow_right_alt),
               label: const Text('Start Quiz'))
